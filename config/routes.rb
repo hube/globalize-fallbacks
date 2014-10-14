357edb74e0b7710d9c50279468f1cbe728b856d9
Hubert Lee <hubert.k.lee@gmail.com>
2014-10-14 17:33:43 +0800

    add more i18n config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  scope "(:locale)", locale: /en|es/ do
+    root 'posts#index'
+
+    resources :posts
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
