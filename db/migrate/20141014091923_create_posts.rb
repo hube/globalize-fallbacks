7424734597198f3d28d25b400886b4e21924111a
Hubert Lee <hubert.k.lee@gmail.com>
2014-10-14 17:23:42 +0800

    add posts model from scaffold

---
+++ db/migrate/20141014091923_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def up
+    create_table :posts do |t|
+      t.timestamps
+    end
+    Post.create_translation_table! title: :string
+  end
+
+  def down
+    drop_table :posts
+    Post.drop_translation_table!
+  end
+end
